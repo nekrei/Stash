a6b83d48a3a4c1f4ed02fe3d0e548267b0f94d55
nekrei <165567276+nekrei@users.noreply.github.com>
2024-11-20 11:21:12 +0600

    Add files via upload

--- linkedlist.c
+++ linkedlist.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 struct list{
     struct list* prev;
@@ -8,24 +9,59 @@ struct list{
     struct list* next;
 };
 
+struct list* begin;
+struct list* end;
 
-void create(double x,double y, struct list** end){
+void create(double x,double y){
     struct list* p = malloc(sizeof(struct list));
-    (*p).prev = *end;
+    (*p).prev = end;
     (*p).next = NULL;
     (*p).x  = x;
     (*p).y = y;
-    (**end).next = p;
-    *end = p; 
+    (*end).next = p;
+    end = p; 
 }
+struct list* find(double x, double y){
+    struct list * it;
+    struct list *ans = NULL;
+    for(it = (*begin).next; it ; it = it->next){
+        if(fabs(x-(it->x)) < 1e-7 && fabs(y- (it->y)) < 1e-7) {ans = it; break;}
+    }
+    return ans;
+}
+//void delete(double x, double y){
+//    struct list *addr = find(x,y);
+//    struct list *b4 = addr->prev;
+//    struct list *n4 = addr->next;
+//    b4->next = n4;
+//    n4->prev = b4;
+//    free(addr);
+//}
+
+void delete(struct list* addr){
+    struct list *b4 = addr->prev;
+    struct list *n4 = addr->next;
+    b4->next = n4;
+    n4->prev = b4;
+    free(addr);
+}
+
 int main(){
-    struct list* begin;
-    struct list* end;
     begin = malloc(sizeof(struct list));
     end = begin;
     
-    create(12.3,23.4,&end);
-    create(11.2, 11.3,&end);
-    create(22, 15.66,&end);
+    create(12.3,23.4);
+    create(11.2, 11.3);
+    create(22, 15.66);
+
+    struct list * it = (*begin).next;
+    for(it = (*begin).next; it ; it = it->next)
+        printf("%lf %lf\n",it->x, it->y);
+
+    delete(find(11.2, 11.3));
+
+    for(it = (*begin).next; it ; it = it->next)
+        printf("%lf %lf\n",it->x, it->y);
+
     return 0;
 }
