67b3958cccb643065302e22029ff7632a1e8c941
M Ahsaf Abid <165567276+nekrei@users.noreply.github.com>
2026-01-14 10:01:57 +0600

    Create flyod0idx.cpp

---
+++ flyod0idx.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const ll INF = 1e15;
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<ll>> am(n+1, vector<ll>(n+1,INF));
+    for(int i=0; i<n; i++)
+        am[i][i] = 0;
+    for(int i=0; i<m; i++){ 
+        int a,b,w;
+        cin>>a>>b>>w;
+        am[a][b]=w;
+    }
+    
+    for(int k=1; k<=n; k++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++)
+                am[i][j] = min(am[i][j], am[i][k] + am[k][j]);
+        }
+    }
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+        solve();
+    return 0;
+}
